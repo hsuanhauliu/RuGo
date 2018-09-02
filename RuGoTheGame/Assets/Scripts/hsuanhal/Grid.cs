ce01002367067b186d17a56469e6b80817d60cd7
hsuanhauliu <howardliu43@gmail.com>
2018-09-01 21:23:58 -0700

    simple scripts for testing grid system and spawning gameobjects

---
+++ RuGoTheGame/Assets/Scripts/hsuanhal/Grid.cs
@@ -0,0 +1,41 @@
+﻿// a sample code from: https://www.youtube.com/watch?v=VBZFYGWvm4A
+
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private float gridSize = 1f;
+	
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / gridSize);
+        int yCount = Mathf.RoundToInt(position.y / gridSize);
+        int zCount = Mathf.RoundToInt(position.z / gridSize);
+
+        Vector3 result = new Vector3(
+            (float)xCount * gridSize,
+            (float)yCount * gridSize,
+            (float)zCount * gridSize);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    // draw visual points on grid for reference. Not working for some reason...
+    private void OnGrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < 40; x += gridSize)
+        {
+            for (float z = 0; z < 40; z += gridSize)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+}
---
+++ RuGoTheGame/Assets/Scripts/hsuanhal/WallInstantiation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallInstantiation : MonoBehaviour
+{
+    public Transform brick;
+
+    // build a wall at the origin
+    void Start()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Instantiate(brick, new Vector3(x, (float)(y + 0.5), 0), Quaternion.identity);
+            }
+        }
+    }
+}
