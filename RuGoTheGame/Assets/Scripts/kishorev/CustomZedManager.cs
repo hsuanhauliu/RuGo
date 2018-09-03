3f8c20ee64c0564c5429c2181bbf0dec6f470f90
Kishore Venkateshan <kishorev@usc.edu>
2018-09-02 22:40:32 -0700

    ZED Package integrated

---
+++ RuGoTheGame/Assets/Scripts/kishorev/CustomZedManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomZedManager : MonoBehaviour {
+
+    private void Awake()
+    {
+        Debug.Log(this.name + " is Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log(this.name + " is Enabled");
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(this.name + " is Started");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
