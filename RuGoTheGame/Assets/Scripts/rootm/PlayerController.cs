a07fedbef0afa4dfa11f496af1f52feb9c3b3685
nemosx <root.michael.r@gmail.com>
2018-09-06 12:49:30 -0700

    Add missing file

--- RuGoTheGame/Assets/Scripts/rootm/PlayerController.cs
+++ RuGoTheGame/Assets/Scripts/rootm/PlayerController.cs
@@ -7,14 +7,14 @@ public class PlayerController : MonoBehaviour {
     public Gadget currentGadget;
 
 
-
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (currentGadget)
         {
             if (Input.GetKeyDown(KeyCode.L))
