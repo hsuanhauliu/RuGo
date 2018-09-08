fa7cb5c4ee71f480078dbde82f14937a62de9a62
hsuanhauliu <howardliu43@gmail.com>
2018-09-08 12:49:15 -0700

    updated working code

--- RuGoTheGame/Assets/Scripts/master/GameManager.cs
+++ RuGoTheGame/Assets/Scripts/master/GameManager.cs
@@ -1,68 +1,171 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager: MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    // References to different classes
+    public GadgetManipulator manipulator;           // manipulator for gadgets
+    public GadgetSelectorMenu gadgetSelectorMenu;   // selector menu
 
-    public GadgetManipulator manipulator;
-
+    // TODO: needs to change it later. manually referencing prefabs for now
     public GameObject box;
 
-    public GadgetSelectorMenu gadgetSelectorMenu;
+    // Local variables
+    private string[] gameModes;
+    private int currentGameMode;
 
-    private List<Gadget> gadgetsInWorld;
+    // Initializer; executed when the game starts
+    void Start()
+    {
+        // TODO: add more game modes in the future
+        gameModes = new string[] {
+            "manipulate",
+            "selectorMenu"
+        };
 
-	void Start () {
-        gadgetsInWorld = new List<Gadget>();
+        // Set up game mode and object active states
+        currentGameMode = 0;                            // set game mode
+        manipulator.gameObject.SetActive(true);         // make sure manipulator is active
+        gadgetSelectorMenu.gameObject.SetActive(false); // make sure it is turned off
+
     }
-	
-    private bool PickingEnabled() {
-        return !gadgetSelectorMenu.isActiveAndEnabled && !manipulator.selectedGadget;
-    }
 
-	void Update () {
-        if (PickingEnabled()) {
-            if (Input.GetMouseButtonDown(0))
+    // Update is called once per frame
+    void Update()
+    {
+        // If we're in manipulate mode...
+        if (currentGameMode == 0)
+        {
+            // Listen for left click
+            if (Input.GetMouseButtonDown(0) && manipulator.GetMode() == 0)
             {
-                //Convert Mouse Screen Coordinates to Ray in 3D Space
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    Gadget gadget = hit.transform.GetComponent<Gadget>();
-                    if (gadget)
-                    {
-                        gadget.Highlight();
-                        manipulator.selectedGadget = gadget;
-                    }
-                }
+                SelectExistingGadget();
+            }
+
+            // Listen for R for reset button
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                this.Reset();
             }
         }
 
+        // Listen for M for menu toggle
         if (Input.GetKeyDown(KeyCode.M))
         {
-            gadgetSelectorMenu.ToggleMenu();
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            this.Reset();
+            // If menu is active, turn it off and change mode to manipulate
+            if (gadgetSelectorMenu.isActiveAndEnabled)
+            {
+                SwitchMode(0);
+            }
+            // If menu is inactive, turn it on and change mode to selectorMenu
+            else
+            {
+                SwitchMode(1);
+            }
         }
     }
 
+
+    /************************** Public Functions **************************/
+
+    //TODO 1. might need to reset future new classes
+    //     2. should be invoked by UI button instead of key press?
+    // Function: Reset
+    // Input: none
+    // Output: none
+    // Description:
+    // - Reset the world to its initial state.
     public void Reset()
     {
-        gadgetsInWorld.ForEach((Gadget g) => g.Reset());
+        //gadgetsInWorld.ForEach((Gadget g) => g.Reset());
     }
 
-    public void CreateGadget(string name) {
+    // Function: CreateGadget
+    // Input: name of the prefab file
+    // Output: none
+    // Description:
+    // - Create a gameObject using prefab. Invoked by UI buttons.
+    public void CreateGadget(string name)
+    {
         //TODO Delegate creation of gadgets to GadgetFactory
         //GadgetFactory factory = GadgetFactory.GetInstance();
         //Gadget gadget = factory.CreateGadget(gadgetName);
-        GameObject d = Instantiate(box, this.transform);
-        Gadget gadget = d.GetComponent<Gadget>();
-        gadgetsInWorld.Add(gadget);
 
-        gadget.Highlight();
-        manipulator.selectedGadget = gadget;
+        //TODO currently placing gadget at gameManager's position.
+        // Might want to place it in front of the player for a set distance.
+        GameObject gadgetObj = Instantiate(box, this.transform);
+        Gadget gadget = gadgetObj.GetComponent<Gadget>();
+        manipulator.Set(gadget, 1);
+    }
+
+    // Function: SwitchMode
+    // Input:
+    // - newMode: code of the new mode we want to switch to
+    // Output: none
+    // Description:
+    // - Switch to a different mode. Rvery time there is a mode change, make
+    //   sure to reset/clean up the previous mode, set it to inactive, and change
+    //   gameMode code.
+    public void SwitchMode(int newMode)
+    {
+        Debug.Log("*** Game mode switched from " + gameModes[currentGameMode] +
+                  " to " + gameModes[newMode] + " ***");
+
+        // Turn off modules associated with the current mode
+        switch (currentGameMode)
+        {
+            case 0:
+                manipulator.Reset();
+                manipulator.gameObject.SetActive(false);
+                break;
+            case 1:
+                gadgetSelectorMenu.gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+
+        // Turn on module associated with the new mode
+        currentGameMode = newMode;
+        switch (newMode)
+        {
+            case 0:
+                manipulator.gameObject.SetActive(true);
+                break;
+            case 1:
+                gadgetSelectorMenu.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    /************************** Private Functions **************************/
+
+    // Function: SelectExistingGadget
+    // Input: none
+    // Output: none
+    // Description:
+    // - Find and select the gadget being clicked on.
+    private void SelectExistingGadget()
+    {
+        // Convert Mouse Screen Coordinates to Ray in 3D Space
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // if we do click on something...
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Create a reference to the gadget that we clicked on
+            Gadget gadget = hit.transform.GetComponent<Gadget>();
+
+            // Check whether the object that we clicked on was a gadget
+            if (gadget)
+            {
+                Debug.Log("A gadget is being selected.");
+                manipulator.Set(gadget, 2);
+            }
+        }
     }
 }
---
+++ RuGoTheGame/Assets/Scripts/master/World.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    // Local variable; keep track of the gadgets in the world
+    private List<Gadget> gadgetsInWorld;
+
+    // Use this for initialization
+    void Start () {
+        gadgetsInWorld = new List<Gadget>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateGadgetFromTemplate (Gadget gadgetTemplate)
+    {
+        GameObject gadgetObj = Instantiate(gadgetTemplate.gameObject, this.transform);
+        Gadget gadget = gadgetObj.GetComponent<Gadget>();
+        gadget.Solidify();
+        gadgetsInWorld.Add(gadget);
+    }
+}
