3aae37d84efd88637761b83138e924402b4b4f3b
nemosx <root.michael.r@gmail.com>
2018-09-14 10:51:10 -0700

    Integrate menu into master scene

--- RuGoTheGame/Assets/Scripts/master/GadgetSelectorMenu.cs
+++ RuGoTheGame/Assets/Scripts/master/GadgetSelectorMenu.cs
@@ -2,14 +2,28 @@
 
 public class GadgetSelectorMenu : MonoBehaviour
 {
+    public GameManager gameManager;
+
     void Start()
     {
+        GameObject gadgetPrefab = Resources.Load("BasicButton") as GameObject;
+        //TODO Add Button to Panel transform instead of Entire Menu
+        GameObject gadgetButton = (GameObject)Instantiate(gadgetPrefab, this.transform);
 
+        UnityEngine.UI.Button uiButton = gadgetButton.GetComponent<UnityEngine.UI.Button>();
+        string buttonIdentifier = GadgetInventory.Ball.ToString();
+        uiButton.GetComponentInChildren<UnityEngine.UI.Text>().text = buttonIdentifier;
+        uiButton.onClick.AddListener(() => SelectGadget(buttonIdentifier));
     }
 
     void Update()
     {
 
+    }
+
+    public void SelectGadget(string selectedGagdget) {
+        gameManager.CreateGadget(selectedGagdget);
+        gameManager.EnableBuildMode();
     }
 
     public void Activate()
--- RuGoTheGame/Assets/Scripts/master/GameManager.cs
+++ RuGoTheGame/Assets/Scripts/master/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("Game Manager Started");
         EnableBuildMode();
     }
 
