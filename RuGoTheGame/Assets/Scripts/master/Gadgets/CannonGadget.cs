60816fda36be565550adef0f4b4e20e0733906e3
nemosx <root.michael.r@gmail.com>
2018-09-16 21:42:25 -0700

    cannon fire with F key

--- RuGoTheGame/Assets/Scripts/master/Gadgets/CannonGadget.cs
+++ RuGoTheGame/Assets/Scripts/master/Gadgets/CannonGadget.cs
@@ -2,6 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CannonGadget : Gadget {
+public class CannonGadget : Gadget
+{
+    private GameObject mCannonBallPrefab;
+
+    private Transform mBarrel;
+
+    private void Start()
+    {
+        mCannonBallPrefab = Resources.Load("CannonBall") as GameObject;
+        mBarrel = this.transform.Find("Wooden_pillow");
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject cannonBall = Instantiate(mCannonBallPrefab, mBarrel);
+            Rigidbody rigidBody = cannonBall.GetComponent<Rigidbody>();
+
+            Vector3 barrelDirection = cannonBall.transform.up;
+            rigidBody.AddForce(barrelDirection * 1.3f, ForceMode.Impulse);
+        }
+
+    }
 
 }
