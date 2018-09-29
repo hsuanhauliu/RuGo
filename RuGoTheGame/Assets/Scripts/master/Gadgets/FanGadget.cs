c291458067c451d7a81dc3e5db95f7fd65c81fea
Devashree Shirude <shirude@usc.edu>
2018-09-28 18:19:01 -0700

    Minor change in fan

--- RuGoTheGame/Assets/Scripts/master/Gadgets/FanGadget.cs
+++ RuGoTheGame/Assets/Scripts/master/Gadgets/FanGadget.cs
@@ -11,6 +11,7 @@ public class FanGadget : Gadget {
     }
 
     private Transform blades;
+    private Transform wind;     private float windStrength = 10;     public float radius = 1;     private int i;     public float windStrengthMin = 5;     public float windStrengthMax = 25;     public Transform windTransformPosition;     public Transform windTransformRotation;
 
     void Start()
     {
@@ -19,9 +20,12 @@ public class FanGadget : Gadget {
 
     void Update()
     {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
         if (Input.GetKey(KeyCode.L))
         {
             blades.Rotate(new Vector3(0, 0, 45));
+            if (windTransformPosition != null && windTransformRotation != null)             {                 RaycastHit hit;                 windStrength = Random.Range(windStrengthMin, windStrengthMax);                 windTransformRotation.rotation = transform.rotation;                  var hitColliders = Physics.OverlapSphere(windTransformPosition.transform.position, radius);                 for (i = 0; i < hitColliders.Length; i++)                 {                     if (hitColliders[i].GetComponent<Rigidbody>() != null)                     {                          var rayDirection = hitColliders[i].GetComponent<Rigidbody>().gameObject.transform.position - windTransformPosition.transform.position;                         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))                          {                             if (hit.transform.GetComponent<Rigidbody>())                             {                                  hitColliders[i].GetComponent<Rigidbody>().AddForce(windTransformPosition.transform.forward * windStrength, ForceMode.Acceleration);                              }                         }                     }                 }             } 
         }
     }
 
--- RuGoTheGame/Assets/Scripts/master/WindRigidbody.js
+++ RuGoTheGame/Assets/Scripts/master/WindRigidbody.js
@@ -1,19 +1,14 @@
-﻿/*
-WindRigidbody v.1.0 by youtube.com/vulgerstal
-How to use
-1) Place 'Wind.Rigidbody' prefab in 0,0,0
-2) Extract and Use 'Wind.Remote.Control'
-*/
+﻿
 private var windStrength : float = 4;
-private var radius : float = 1234567;
+public var radius : float = 2;
 private var i : int;
-var windStrengthMin : float = 0;
-var windStrengthMax : float = 5;
+var windStrengthMin : float = 10;
+var windStrengthMax : float = 30;
 var windTransformPosition : Transform;
 var windTransformRotation : Transform;
 function Update()
 {
-	if(windTransformPosition != null && windTransformRotation != null)
+	/*if(windTransformPosition != null && windTransformRotation != null)
 	{
 		windStrength = Random.Range(windStrengthMin, windStrengthMax);
 		windTransformRotation.rotation = transform.rotation;
@@ -25,16 +20,16 @@ function Update()
 					{
 					var hit : RaycastHit;
 					var rayDirection = hitColliders[i].GetComponent.<Rigidbody>().gameObject.transform.position - windTransformPosition.transform.position;
-						if(Physics.Raycast(windTransformPosition.transform.position, rayDirection, hit)) //there was ',hit, 100' is from an old test.
+						if(Physics.Raycast(windTransformPosition.transform.position, rayDirection, hit)) 
 						{
 							if(hit.transform.GetComponent.<Rigidbody>())
 							{
-								//AddExplosionForce(512, transform.position, radius, 3.0); //More garbage from old tests . . . 
+
 								hitColliders[i].GetComponent.<Rigidbody>().AddForce(windTransformPosition.transform.forward * windStrength,ForceMode.Acceleration);
-								//there was '32' instead of windStrength //just a note for myself.
+
 							}
 						}	
 					}
 				}
-	}
+	}*/
 }
