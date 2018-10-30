b4d09e3ffc889b47312147ef57eb35fe5d29256b
NiwradMendyke <mendyke@usc.edu>
2018-10-30 15:13:41 -0700

    adding sounds and new audioObject script

---
+++ RuGoTheGame/Assets/Scripts/master/Gadgets/AudioObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour {
+	
+    protected AudioSource mAudioData;
+
+    new void Start()
+    {
+        mAudioData = this.transform.parent.GetComponent<AudioSource>();
+    }
+
+	void OnCollisionEnter(Collision col)
+    {
+        // print("\ncalling OnCollisionEnter with " + this.name + " and " + col.gameObject.name);
+        if (this.name == "BallGeo") {
+            if(col.gameObject.name == "BallGeo" 
+                || col.gameObject.name == "BoxBody" 
+                || col.gameObject.name == "button"
+                )
+            {
+                mAudioData.Play(0);
+            }
+        }
+        if (this.name == "DominoBody") {
+            if(col.gameObject.name == "DominoBody")
+            {
+                mAudioData.Play(0);
+            }
+        }
+    }
+}
