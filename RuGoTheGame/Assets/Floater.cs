f0a91b6b3eedaf7b1732f9d3c2c0e54aa7b6cd0c
Devashree Shirude <shirude@usc.edu>
2018-11-11 02:06:09 -0800

    Floater gadget - to be experimented with

---
+++ RuGoTheGame/Assets/Floater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
