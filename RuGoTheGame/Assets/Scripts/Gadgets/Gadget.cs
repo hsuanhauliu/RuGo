0c3be838a154d2e8b7cac9c1bfdedb4c98d9c406
nemosx <root.michael.r@gmail.com>
2018-11-12 17:03:52 -0800

    Add LightMarble gadget. Tested in Sim Only

--- RuGoTheGame/Assets/Scripts/Gadgets/Gadget.cs
+++ RuGoTheGame/Assets/Scripts/Gadgets/Gadget.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 
 public enum GadgetInventory
 {
-    Ramp, Ball, Goal, Box, Cannon, Spinner, Fan, Airplane, Domino, Pendulum, Floater, NUM // #TODO: If time permits, split the heirarchy to Insertable gadgets and Tools gadgets for LoadCube
+    Ramp, Ball, LightMarble, Goal, Box, Cannon, Spinner, Fan, Airplane, Domino, Pendulum, Floater, NUM // #TODO: If time permits, split the heirarchy to Insertable gadgets and Tools gadgets for LoadCube
 }
 
 public class GadgetLayers
---
+++ RuGoTheGame/Assets/Scripts/Gadgets/LightMarble.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMarble : BallGadget {
+
+    public override GadgetInventory GetGadgetType()
+    {
+        return GadgetInventory.LightMarble;
+    }
+}
