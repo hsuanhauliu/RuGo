be8eac961bb77d2846bed085ba6817d2b285ec00
Kishore Venkateshan <kishorev@usc.edu>
2018-11-16 13:51:38 -0800

    Changed ramp prefab to be straight model as opposed to be the curved one. Because the convex physics isn't curved

--- RuGoTheGame/Assets/Scripts/Gadgets/LowRampGadget.cs
+++ RuGoTheGame/Assets/Scripts/Gadgets/LowRampGadget.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RailRampGadget : Gadget
+public class LowRampGadget : Gadget
 {
     public override GadgetInventory GetGadgetType()
     {
-        return GadgetInventory.Ramp;
+        return GadgetInventory.LowRamp;
     }
 }
---
+++ RuGoTheGame/Assets/Scripts/Gadgets/MedRampGadget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedRampGadget : Gadget
+{
+    public override GadgetInventory GetGadgetType()
+    {
+        return GadgetInventory.MedRamp;
+    }
+}
+
