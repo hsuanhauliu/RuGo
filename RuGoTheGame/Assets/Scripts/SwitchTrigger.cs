4e3d894a44cb572da08c29664d1d983206516eb0
nemosx <root.michael.r@gmail.com>
2018-11-16 09:01:18 -0800

    Prevent Triggers from activating due to Floater

--- RuGoTheGame/Assets/Scripts/SwitchTrigger.cs
+++ RuGoTheGame/Assets/Scripts/SwitchTrigger.cs
@@ -42,6 +42,14 @@ public class SwitchTrigger : MonoBehaviour {
         {
             Collider collisionCollider = collision.collider;
             Gadget otherGadget = collisionCollider.gameObject.GetComponentInParent<Gadget>();
+
+
+            // Avoid trigger due to collision with Floating Platform
+            if (otherGadget is Floater) {
+                return;
+            }
+
+
             VRTK.VRTK_PlayerObject isPlayer = collisionCollider.gameObject.GetComponentInParent<VRTK.VRTK_PlayerObject>();
             if ((otherGadget != null && otherGadget.GetPhysicsMode()) || isPlayer != null)
             {
