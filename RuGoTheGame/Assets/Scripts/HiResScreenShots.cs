f8df7c46e8e98ef73fec19ed98083f4ef22ccdc8
Abhishek Bhatt <abhishek@Abhisheks-MacBook-Pro.local>
2018-11-18 10:45:00 -0800

    Refactored screenshot code

--- RuGoTheGame/Assets/Scripts/HiResScreenShots.cs
+++ RuGoTheGame/Assets/Scripts/HiResScreenShots.cs
@@ -1,43 +1,55 @@
 ﻿using UnityEngine;
- using System.Collections;
- 
- public class HiResScreenShots : MonoBehaviour {
-     public int resWidth = 1024; 
-     public int resHeight = 768;
-    //public new Camera MainCamera;
-    public new Camera camera;
-     private bool takeHiResShot = false;
- 
-     public static string ScreenShotName(int width, int height) {
-         return string.Format("screenshots/screen_{0}x{1}_{2}.png", 
-                              width, height, 
-                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
-     }
- 
-     public void TakeHiResShot() {
-         takeHiResShot = true;
-     }
- 
-     void LateUpdate() {
-         takeHiResShot |= Input.GetKeyDown("k");
-         if (takeHiResShot) {
-             RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-             camera.targetTexture = rt;
-             Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-             camera.Render();
-             RenderTexture.active = rt;
-             screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-             camera.targetTexture = null;
-             RenderTexture.active = null; // JC: added to avoid errors
-             Destroy(rt);
-             byte[] bytes = screenShot.EncodeToPNG();
-             string filename = ScreenShotName(resWidth, resHeight);
-             System.IO.File.WriteAllBytes(filename, bytes);
-             Debug.Log(string.Format("Took screenshot to: {0}", filename));
-             takeHiResShot = false;
-            //camera.enabled = true;
-            // MainCamera.enabled = true;
-         }
-     }
+using System.Collections;
 
- }
+public class HiResScreenShots : MonoBehaviour
+{
+    public int resWidth = 1024;
+    public int resHeight = 768;
+    private Camera mScreenShotCamera;
+    //public  Camera camera;
+
+    private string SCREENSHOT_SAVE_DIR = "screenshots/";
+
+    public static string CreateScreenshotFilename(int width, int height)
+    {
+        return string.Format("screen_{0}x{1}_{2}.png",
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    private void Awake()
+    {
+        mScreenShotCamera = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+
+            mScreenShotCamera.targetTexture = rt;
+
+            mScreenShotCamera.Render();
+
+            RenderTexture.active = rt;
+
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+
+            // Read pixels from the screen
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+
+            // Do clean up
+            mScreenShotCamera.targetTexture = null;
+            RenderTexture.active = null; // nullify active to avoid error
+            Destroy(rt);
+
+            // Write the screenshot file
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filePath = SCREENSHOT_SAVE_DIR + CreateScreenshotFilename(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filePath, bytes);
+            Debug.Log(string.Format("Saved screenshot to: {0}", filePath));
+        }
+    }
+
+}
